30f18544dcd3c9ab652ab81aaca13b721a8b06d9
Christine Grimadeau <cgrimad1@yahoo.com>
2026-06-06 12:54:05 -0400

    Added redirect links

--- Silicon Alley List/src/App.jsx
+++ Silicon Alley List/src/App.jsx
@@ -16,13 +16,15 @@ const App = () => {
           image="/Northslope_Icon.avif"
           company="Northslope"
           title="Software Engineer, New Grad"
+          link="https://www.builtinnyc.com/job/forward-deployed-software-engineer-new-grad/8697606"
           location="New York, NY (Hybrid)"
           />
 
           <Card 
           image="/Pinterest.svg.png"
           company="Pinterest"
-          title="Frontend Engineer, New Grad"
+          title="Fall Machine Learning Internship (Masters)"
+          link="https://www.pinterestcareers.com/jobs/7253017/masters-fall-machine-learning-internship-atg-visual-search/?gh_jid=7253017"
           location="11 Madison Ave, New York, NY 10010 (Hybrid)"
           />
 
@@ -30,6 +32,7 @@ const App = () => {
           image="/Spotify-Icon-Logo.svg"
           company="Spotify"
           title="Backend Engineer, Podcast"
+          link="https://www.lifeatspotify.com/jobs/backend-engineer-podcast-2"
           location="150 Greenwich St, New York, NY 10007 (Hybrid)"
           />
 
@@ -37,6 +40,7 @@ const App = () => {
           image="/Braze_Social_Profile.avif"
           company="Braze"
           title="IT Associate"
+          link="https://wellfound.com/jobs/4307519-it-associate"
           location="63 Madison Ave 13th Floor, New York, NY 10016"
           />
 
