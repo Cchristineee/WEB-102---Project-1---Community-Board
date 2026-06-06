e9a9b65f014400ba30b3c720326b0072391fd3c9
Christine Grimadeau <cgrimad1@yahoo.com>
2026-06-06 12:27:50 -0400

    made changes to sizing of cards and spacing

--- Silicon Alley List/src/Components/Card.jsx
+++ Silicon Alley List/src/Components/Card.jsx
@@ -5,10 +5,11 @@ const Card = ({ image, company, title, location }) => {
     return (
         <div className="Card"> 
             {image && <img className="card-image" src={image} alt={`${company} logo`} />}
-            <h2 className="company-name">{company}</h2>
-            <h3 className="job-title">{title}</h3>
-            <h4 className="job-description">{location}</h4>
-
+            <div className="card-body">
+              <h2 className="company-name">{company}</h2>
+              <h3 className="job-title">{title}</h3>
+              <h4 className="location">{location}</h4>
+            </div>
             <button className="Learn-more-button">Learn More</button>
         </div>
     )
