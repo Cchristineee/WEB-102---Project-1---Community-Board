2442a4371d66bc0d70b5acf3ea34ac0fdeb33d34
Christine Grimadeau <cgrimad1@yahoo.com>
2026-06-06 12:53:14 -0400

    Added link prop for company links

--- Silicon Alley List/src/Components/Card.jsx
+++ Silicon Alley List/src/Components/Card.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import './Card.css'
 
-const Card = ({ image, company, title, location }) => {
-    return (
-        <div className="Card"> 
-            {image && <img className="card-image" src={image} alt={`${company} logo`} />}
-            <div className="card-body">
-              <h2 className="company-name">{company}</h2>
-              <h3 className="job-title">{title}</h3>
-              <h4 className="location">{location}</h4>
-            </div>
-            <button className="Learn-more-button">Learn More</button>
-        </div>
-    )
+const Card = ({ image, company, title, location, link }) => {
+        return (
+                <div className="Card"> 
+                        {image && <img className="card-image" src={image} alt={`${company} logo`} />}
+                        <div className="card-body">
+                            <h2 className="company-name">{company}</h2>
+                            <h3 className="job-title">{title}</h3>
+                            <h4 className="location">{location}</h4>
+                        </div>
+                        {link ? (
+                            <a className="Learn-more-button" href={link} target="_blank" rel="noopener noreferrer">Learn More</a>
+                        ) : (
+                            <button className="Learn-more-button">Learn More</button>
+                        )}
+                </div>
+        )
 }
 
 export default Card
